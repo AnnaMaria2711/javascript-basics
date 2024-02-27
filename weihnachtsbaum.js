bc16f6015ed0dc2beb185991e808ad16840a4b66
AnnaMaria2711 <Asnna-Maria.Buelk@coop.ch>
2024-02-27 11:20:32 +0100

    Coded weihnachtsbaum.js

---
+++ weihnachtsbaum.js
@@ -0,0 +1,11 @@
+let n = 5;
+
+    for(let i = 0; i < n; i++){
+        let Zeile = ' '.repeat(n-i) + "*".repeat(i*2+1)
+        console.log(Zeile) }
+
+        for(let i = 0; i < n; i++){
+        let Leerschlag= ' '.repeat(n) + "*"
+        console.log(Leerschlag)
+
+    }
